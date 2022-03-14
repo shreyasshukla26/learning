1de4c9642d9eb0ecdf84c3f50355d9ee41a81ff8
shreyasshukla26 <shuklashreyas666@gmail.com>
2022-03-15 01:04:59 +0530

    intro to js

---
+++ module 2/intro.js
@@ -0,0 +1,18 @@
+console.log("hey sup budyyy!!!!!")   // print in js
+let a = 500 ;   // print varibale in js 
+console.log(a); // str print 
+let xo = 'i am a string'; 
+console.log(xo);
+
+
+// loops 
+//while loop 
+let count = 10;
+while(count>0){
+    console.log(count);
+    count--;
+}
+//for loop
+for (let i =1; i <=5;i++){
+    console.log(i);
+}
